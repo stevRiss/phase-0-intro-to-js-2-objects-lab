79880aa2f82264ac0f2623f3c844e50a2cab4ea5
Stephen <stevriss22@gmail.com>
2021-09-07 23:15:15 -0400

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // Write your solution in this file!
+
+const employee = {
+    name: "jiffy",
+    streetAddress: "jiffy lane",
+}
+
+beforeEach(function () {
+    for (const key in employee) {
+      delete employee[key];
+    }
+
+    employee.name = 'Sam';
+  });
+
+function updateEmployeeWithKeyAndValue(employee, key, value){
+    const newObj = {...employee};
+    newObj[key]= value;
+    return newObj;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+    employee[key]=value;
+    return employee;
+}
+ const newEmp = destructivelyUpdateEmployeeWithKeyAndValue(employee, "Sam", "11 Broadway");
+ newEmp;
+
+ function deleteFromEmployeeByKey(employee, key){
+    const {name, ...newEmployee} = employee;       //creates clone(newEmployee) of employee & will name name before it has been declared in employee
+    return newEmployee;
+ }
+
+ function destructivelyDeleteFromEmployeeByKey(employee, key){
+     
+     employee[key] = undefined;
+    return employee;
+ }
+const newEmployee = destructivelyDeleteFromEmployeeByKey(employee, "sam" );
+
